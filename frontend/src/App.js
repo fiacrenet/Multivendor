58bff3b7d43fa1fe396db19558043ac4fc3c3012
fiacrenet <fiacreyatta@gmail.com>
2024-11-24 02:34:19 +0100

    latest product section done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,16 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Navbar from './components/navbar/Navbar'
+import LatestProduct from './components/latest_product/LatestProduct'
 
-function App() {
+const App = () => {
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-        <div className="container">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a className="navbar-brand" href="#">Market Place</a>
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Product</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Brand</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Category</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control ms-5" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-  );
+    <div>
+      <Navbar />
+      <LatestProduct />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/latest_product/LatestProduct.jsx
@@ -0,0 +1,136 @@
+import React from 'react'
+
+function LatestProduct() {
+  return (
+    <div className="latesproduct mt-4">
+        <div className="container">
+            <h3 className='mb-4'>Latest Products <a href='#' className='float-end btn  btn-dark mt-2'>View All Products <i class="fa-solid fa-arrow-right"></i></a></h3>
+            <div className="row">
+                {/* Product Box */}
+                <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSF2qx_OjGWuvycRSt-a7qYie-WLmNXrQ5KFADqgIj2sTsD-0C34Jo67QQujjDf879UcBsnge3LWoRvRbSfNUVxepsSSCkgTVxR9gIktKPE4A7YU0oJo_Pt4Xw-Jm43XXPKhpziXA&usqp=CAc' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+                {/* Product Box */}
+                <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://www.hkoenig.com/img/211/292629/4/m3/p/mini-four-electrique-25-l.jpg' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+                {/* Product Box */}
+                <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ9EU-KR1frKLgapx88pqFwpEBm-D7eiOhf_PzKoCne1RiZYY5SdQ-sebTUaGKWTkluzakfPDQ_KBNv-Ohedr9piW-kO51GDrYjURGE4MjSgQh0q1rZsofXRcZ5z57PpQP0J6IMUg&usqp=CAc' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+                {/* Product Box */}
+                <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWYcnziSDYQ3Y4-LQ0RiuA8f_btbBLATlWtQ&s' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+                 {/* Product Box */}
+                 <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSdVubl9IvN6VbCjUlMo3UdZ5ZQzvjwJnGAQUo75nrg402DPLam25oTNjOE2j5WRDU8qgD_Vflvsh1o1jeaxuVBVZZsgrp4qdY4EQWHAOdiEG6QQ1pwlCJKV0zjlzZsijQY6RGI9Uk&usqp=CAc' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+                 {/* Product Box */}
+                 <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSSb9ZWrl47MWq2Q8mL3r4thY5ZU1wZjfuC1zbuWILs_YWRKSkRsHDfzgXxgEpuk2ZKc-ivw6sBCnWXy00eBtffJXyw335fD1WvzcGaceS8UGv1oA_0guqQ2BlawLaRxCwme5GQDw&usqp=CAc' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+                 {/* Product Box */}
+                 <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRNQ3O6rdY4ItO9J7mkLHAXw2JDnUJHZI47vPqpIy_VLmrpn4pmk63bG5hrK7z_k230mflONsAhqlftno46k2FKtxNkkVpdHPKBi18uVL7O5iDVQVykUO4ze1cZA3M0F-B9FQXpWw&usqp=CAc' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+                 {/* Product Box */}
+                 <div className="col-12 col-md-3 mb-4">
+                    <div className="card">
+                        <img src='https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTWdiBBcsqiKyvni72Dp1G1Ur3g3qsUSjauPFs5fwFyZBQG8vrg_04vyi3VBwB5ncp3ZIGkxg8peesWUiYeUb-Ub3ncqOC7GA8MQL_MmWBFcZwKsPp8xZrS06dHDCCbqA5yQ6nqjrQ&usqp=CAc' className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h4 className="card-title text-center">Product title</h4>
+                            <h5 className="card-title text-muted text-center">Price : € 250 </h5>
+                        </div>
+                        <div className="card-footer">
+                         <button title='Add to cart' className='btn btn-success btn-sm '><i className="fa-solid fa-cart-plus"></i></button>
+                         <button title='Add to wishlist' className='btn btn-danger btn-sm ms-2  '><i className="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>  
+                {/* Product Box End */}
+            </div>     
+        </div>
+    </div>
+      
+  )
+}
+
+export default LatestProduct
